22f265654d217b80bca114d3d1cddbcbee92bea8
wangqian <1802829924@qq.com>
2025-03-06 23:06:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="test">dddd</div>
+    <Header></Header>
+  </div>
+</template>
+
+<script setup>
+import Header from './components/header.vue'
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/header.vue
@@ -0,0 +1,7 @@
+<template><div class="header">header</div></template>
+
+<style lang="scss" scoped>
+.header {
+  color: red;
+}
+</style>
